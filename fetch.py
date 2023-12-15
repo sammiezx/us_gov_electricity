ed738f76084fbabbaf3898bde681ea1e18793ce6
samiricium <sameerpantz@gmail.com>
2023-12-15 10:44:25 +0545

    env

--- fetch.py
+++ fetch.py
@@ -1,21 +1,16 @@
 import requests
+import pandas as pd
+import os
+from dotenv import load_dotenv
 
-start_date = '2001-01'
-end_date = '2023-01'
-offset = 0
-length = 5000
+def hit_gov_data(start_date='2001-01', end_date='2023-01', offset=0, length=5000):
+    load_dotenv()
+    api_key = os.getenv("GOV_API_KEY")
 
-api_url = f"https://api.eia.gov/v2/electricity/retail-sales/data/?frequency=monthly&data[0]=customers&data[1]=price&data[2]=revenue&data[3]=sales&start={start_date}&end={end_date}&sort[0][column]=period&sort[0][direction]=desc&offset={offset}&length={length}"
-
-response = requests.get(api_url)
-
-# Check if the request was successful (status code 200)
-if response.status_code == 200:
-    # Parse and work with the response data (assuming it's in JSON format)
-    data = response.json()
-    
-    # Example: Print the data
-    print(data)
-else:
-    # Print an error message if the request was not successful
-    print(f"Error: {response.status_code} - {response.text}")
+    api_url = f"https://api.eia.gov/v2/electricity/retail-sales/data/?api_key={api_key}&frequency=monthly&data[0]=customers&data[1]=price&data[2]=revenue&data[3]=sales&start={start_date}&end={end_date}&sort[0][column]=period&sort[0][direction]=desc&offset={offset}&length={length}"
+    # api_url = "https://api.eia.gov/v2/electricity/retail-sales/data/?frequency=monthly&data[0]=customers&data[1]=price&data[2]=revenue&data[3]=sales&sort[0][column]=period&sort[0][direction]=desc&offset=0&length=5000"
+    response = requests.get(api_url)
+    if response.status_code == 200:
+        return pd.DataFrame(response.json()['response']['data'])
+    else:
+        raise Exception("ISSUE HITTING US GOVERMENT DATA API")
